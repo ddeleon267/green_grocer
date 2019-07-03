18d0871362bf2fcc14f65ec3035573c1c06ca9fa
github username <github email address>
2019-07-03 15:20:00 -0400

    Update apply_clearance so that it 'only applies coupons that meet minimum amount'

--- grocer.rb
+++ grocer.rb
@@ -17,26 +17,26 @@ def consolidate_cart(cart) # if you don't iterate through the item
   consolidated_cart
 end
 
-def consolidate_cart(cart) # if you iterate through the item
-  consolidated_cart = {}
-
-  cart.each do |item|
-    item.each do |name, details|
-      if consolidated_cart[name]
-        consolidated_cart[name][:count] += 1
-      else
-        consolidated_cart[name] = details
-        consolidated_cart[name][:count] = 1
-      end
-    end
-  end
-  consolidated_cart
-end
+# def consolidate_cart(cart) # if you iterate through the item
+#   consolidated_cart = {}
+#
+#   cart.each do |item|
+#     item.each do |name, details|
+#       if consolidated_cart[name]
+#         consolidated_cart[name][:count] += 1
+#       else
+#         consolidated_cart[name] = details
+#         consolidated_cart[name][:count] = 1
+#       end
+#     end
+#   end
+#   consolidated_cart
+# end
 
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
-    if cart[coupon[:item]]
+    if cart[coupon[:item]] && cart[coupon[:item]][:count] >= coupon[:num]
       if cart["#{coupon[:item]} W/COUPON"]
         cart["#{coupon[:item]} W/COUPON"][:count] += coupon[:num]
 
@@ -54,7 +54,6 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-
   cart.each do |item_name, values|
     if values[:clearance]
       values[:price] = (values[:price] * 0.8).round(2)
@@ -63,5 +62,13 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  # code here
+  consolidated_cart = consolidate_cart(cart)
+  couponed_cart = apply_coupons(consolidated_cart, coupons)
+  final_cart = apply_clearance(couponed_cart)
+
+  total = 0
+  final_cart.each do |item_name, details|
+    total += details[:price]*details[:count]
+  end
+  total
 end
