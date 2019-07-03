48aa05ab0853ae0b6aaf572ab06c5c5224d09e6c
github username <github email address>
2019-07-03 14:04:38 -0400

    First-pass solution for consolidate_cart.

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,21 @@
+require "pry"
 def consolidate_cart(cart)
   consolidated_cart = {}
+
+  cart.each do |item|
+    item_name = item.keys.first
+    if consolidated_cart[item_name]
+      consolidated_cart[item_name][:count] += 1
+    else
+      consolidated_cart[item_name] = {
+        price: item[item_name][:price],
+        clearance: item[item_name][:clearance],
+        count: 1
+      }
+    end
+
+  end
+  consolidated_cart
 end
 
 def apply_coupons(cart, coupons)
